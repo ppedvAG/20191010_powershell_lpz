1dc85cb73990439dbc9cf986767f07441a203764
JoeVice <DanielWill@users.noreply.github.com>
2019-10-10 11:40:32 +0200

    Create get-command-get-help-get-module.ps1

---
+++ get-command-get-help-get-module.ps1
@@ -0,0 +1,8 @@
+﻿get-command -Verb Get # Alle Commandlets mit dem Verb "Get"
+get-command -Noun *AD* # Commandlets welche im Substantiv AD beinhalten
+get-help get-command # Hilfe zum Commandlets "get-command" anzeigen
+get-help get-command -Examples # Beispiele für Commandlets anzeigen
+
+get-command -Module ModulName # Commandlets eines Moduls anzeigen
+get-module # importierte Module anzeigen
+get-module -ListAvailable # verfügbare Module anzeigen (nicht importierte)
