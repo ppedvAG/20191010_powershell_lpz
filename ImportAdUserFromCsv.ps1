f299033f9fb73c0f968d6f35bbadac73610d7949
JoeVice <DanielWill@users.noreply.github.com>
2019-10-10 16:37:13 +0200

    confirm_preference / importaduser

---
+++ ImportAdUserFromCsv.ps1
@@ -0,0 +1,10 @@
+﻿Import-Csv -Path C:\Testfiles\MyNewAdUsers2.csv | ForEach-Object {
+            New-ADUser -GivenName $_.GivenName `
+                       -Surname $_.SurName `
+                       -Name $_.Name `
+                       -SamAccountName $_.SamAccountName `
+                       -Department $_.Department `
+                       -Path $_.Path `
+                       -Enabled $true `
+                       -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -Force)
+}
---
+++ confirm_preference.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+
+$Error.Clear()
+
+Get-ChildItem C:\einPfad
+
+$Error.FullyQualifiedErrorId | Out-File c:\temp\log.log
+
+Write-Host "Nachdem ein Fehler passiert ist"
