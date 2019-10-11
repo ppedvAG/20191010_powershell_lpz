7a0ffd865f4b8776454e2a7eeda88e79dc6628d9
JoeVice <DanielWill@users.noreply.github.com>
2019-10-11 10:43:01 +0200

    Create variablen.ps1

---
+++ variablen.ps1
@@ -0,0 +1,26 @@
+﻿$einObjekt = New-Object -TypeName PSCustomObject
+$einObjekt | Add-Member -MemberType NoteProperty -Name Eigenschaft -Value "Wert"
+$einObjekt | Add-Member -MemberType NoteProperty -Name EigenschaftZwei -Value 10
+
+
+[int]$integerVariable = 5 # explizite Datentypangabe <- [int]
+[string]$stringVariable = "Eine Textfolge"
+[char]$charVariable = 'c'
+
+$unbestimmteVariable = 'c' # obwohl nur ein char angegeben, wird datentyp der variable "string"
+$unbestimmteVariable = 12345
+
+
+
+$integerVariable = "Eine Textfolge" # funktioniert nicht .. Konvertierung schlägt fehl
+$stringVariable = 5 # implizie Konvertierung in string
+
+$integerVariable = "10" # funktioniert .. implizites parsing
+
+
+[string]$eineStringVariable = "Hallo, ich bin eine Stringvariable"
+
+Write-Host "In meiner Variablen vom Typen String steht: $eineStringVariable"
+
+# Variablen wrappen, um Ausgabe von Eigenschaftswerten innerhalb eines Strings zu ermöglichen
+Write-Host "Ich habe ein Objekt mit zwei Eigenschaften. Der erste Wert lautet: $($einObjekt.Eigenschaft), der zweite Wert lautet: $($einObjekt.EigenschaftZwei)" 
